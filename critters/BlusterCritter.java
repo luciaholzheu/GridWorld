05a8529f18d2c5e2d5bacc84bebd8eb608d5b7d5
Cox <truelonghornfan@comcast.net>
2014-02-06 09:57:45 -0700

    Completed Blustr critter, starting on crabs

--- critters/BlusterCritter.java
+++ critters/BlusterCritter.java
@@ -14,38 +14,58 @@ public class BlusterCritter extends Critter {
 		super();
 		courage = 3;
 		this.c = c;
+
+		if (c <= courage) {
+			lighten();
+		}
+		if (c > courage) {
+			darken();
+		}
 	}
 
 	public ArrayList<Actor> getActors() {
 		ArrayList<Actor> actors = new ArrayList<Actor>();
-
 		Location loc = getLocation();
 		for (int column = loc.getCol() - 2; column<= (loc.getCol() + 2); column++) {
 			for (int row = loc.getRow() - 2; row <= (loc.getRow() + 2); row++) {
 				Location test = new Location(row, column);
-				if (getGrid().isValid(test)) {
-					if (getGrid().get(test) instanceof Critter) {
-						c++;
-					}
+				if (getGrid().isValid(test) && getGrid() != null) {
+					Actor a = this.getGrid().get(test);
+					if (a != null && a != this) {
+						actors.add(a);
+					}	
+					
 				}				
 			}
 		}
-		return getGrid().getNeighbors(getLocation());
+		return actors;
 	}
 
-	public void processActors() {
+	public void processActors(ArrayList<Actor> actors) {
+		int courage = 1;
+		int neighbors = 0;
+
+		System.out.println(neighbors);
+		System.out.println(" ");
+		System.out.println(courage);
+
+		for (Actor a : actors ) {
+			if (a instanceof Critter) {
+				neighbors++;
+			}			
+		}
+		if (neighbors < courage) {
+			lighten();
+		}else {
+			darken();
+		}
 		
-			if (c <= courage) {
-				lighten();
-			}else if (c > courage) {
-				darken();
-			}
 		
 	}
 
 
 	public void darken(){
-		DARKENING_FACTOR = 0.1;
+		DARKENING_FACTOR = 0.05;
 		Color c = getColor();
 		int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
 		int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
@@ -55,11 +75,24 @@ public class BlusterCritter extends Critter {
 	}
 
 	public void lighten() {
-		DARKENING_FACTOR = 0.1;
 		Color c = getColor();
-		int red = (int) (c.getRed() * (1 + DARKENING_FACTOR));
-		int green = (int) (c.getGreen() * (1 + DARKENING_FACTOR));
-		int blue = (int) (c.getBlue() * (1 + DARKENING_FACTOR));
+
+		int red = c.getRed();
+		int blue = c.getBlue();
+		int green = c.getGreen();
+
+		
+
+		if (red < 205) {
+			red+=50;
+		}
+		if (blue < 205) {
+			blue+=50;
+		}
+		if (green < 205) {
+			green+=50;
+		}
+
 
 		setColor(new Color(red, green, blue));
 	}
